a8c8aa677033fd44a49d7a35784302891593a998
mahendar-07 <mahendar10019@gmail.com>
2026-03-26 23:41:09 +0530

    MathLibrary

---
+++ javascript/MathLibrary/Mathround.js
@@ -0,0 +1,16 @@
+console.log(`Math.round()`)
+console.log(`Math.round() gives that if <0.5 lower value if =>0.5 upper value`)
+console.log(`Math.round(4.0) is ${Math.round(4.0)}`)
+console.log(`Math.round(4.1) is ${Math.round(4.1)}`)
+console.log(`Math.round(4.2) is ${Math.round(4.2)}`)
+console.log(`Math.round(4.3) is ${Math.round(4.3)}`)
+console.log(`Math.round(4.4) is ${Math.round(4.4)}`)
+console.log(`Math.round(4.5) is ${Math.round(4.5)}`)
+console.log(`Math.round(4.6) is ${Math.round(4.6)}`)
+console.log(`Math.round(4.7) is ${Math.round(4.7)}`)
+console.log(`Math.round(4.8) is ${Math.round(4.8)}`)
+console.log(`Math.round(4.9) is ${Math.round(4.9)}`)
+
+
+
+
