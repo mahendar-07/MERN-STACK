e40fa4afa57eb30829f1ae70fc723e408ef00996
mahendar-07 <mahendar10019@gmail.com>
2026-04-14 23:40:10 +0530

    aa

--- Angular/Project/project/src/app/app.module.ts
+++ Angular/Project/project/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    HomeComponent,
+    DashboardComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
